45ffec18f89d670df90d6c32b515c5d328a9c945
Kristen Michelle Nader <61286548+kris-nader@users.noreply.github.com>
2021-03-08 19:58:52 +0100

    Update GA.py

--- genetic_algo/GA.py
+++ genetic_algo/GA.py
@@ -6,6 +6,8 @@ Created on Sun Mar  7 10:45:39 2021
 """
 import random
 import pandas as pd
+import math
+
 aa=["A","R","N","D","C","E","Q","G","H",
     "I","L","K","M","F","P","S","T","W","Y","V"]
 
@@ -75,9 +77,8 @@ def selection(peptides,interaction_energy,n):
     # fitness: interaciton energy
     # n is the number of peptides to be selected
     # perform a roulette selection
-
     nselected=0
-    percentages=[f/sum(abs(interaction_energy)) for f in abs(interaction_energy)]
+    percentages=[f/sum(abs(interaction_energy)) for f in abs(interaction_energy)] ##??
     selectedIndividuals = []
     while nselected!=n:
         for i in range(0, len(interaction_energy)):
@@ -85,32 +86,69 @@ def selection(peptides,interaction_energy,n):
                 break
             x=random.random()
             if (x<=percentages[i]):
-                selectedIndividuals.append(peptides[i])
+                selectedIndividuals.append(list(peptides)[i])
                 nselected=nselected+1
-        
     return selectedIndividuals
 
+
+## this one will split in a difffernt way
+### pep1: 'R-AT-PFV'  lets say CuttingPoint=1  child1: 'R-NP-PFV'  
+### pep2: 'S-NP-ELY'           CuttingPoint2=3 child2: 'S-AT-ELY'
 def crossover(peptides):
     children = []
     #data.peptideseq
-    tmp = len(peptides)%2
-    for i in range(0,len(peptides)-tmp,2):
+    for i in range(0,len(peptides),2):
         prob = random.random()
         if prob<=crossoverProbability:
             cuttingPoint=random.randint(0, nvar-1)
             cuttingPoint2=random.randint(0, nvar-1)
             child1= peptides[i][0:cuttingPoint] + peptides[i+1][cuttingPoint:cuttingPoint2] + peptides[i][cuttingPoint2:]
             child2= peptides[i+1][0:cuttingPoint] + peptides[i][cuttingPoint:cuttingPoint2] + peptides[i+1][cuttingPoint2:]
-            if(len(child1)==6 and len(child2)==6):
+            if(len(child1)>6 or len(child2)>6):
+                while( len(child1)>6 or len(child2)>6):
+                    cuttingPoint=random.randint(0, nvar-1)
+                    cuttingPoint2=random.randint(0, nvar-1)
+                    child1= peptides[i][0:cuttingPoint] + peptides[i+1][cuttingPoint:cuttingPoint2] + peptides[i][cuttingPoint2:]
+                    child2= peptides[i+1][0:cuttingPoint] + peptides[i][cuttingPoint:cuttingPoint2] + peptides[i+1][cuttingPoint2:]
                 children.append(child1)
                 children.append(child2)
-                i=i-1
+            else:
+                children.append(child1)
+                children.append(child2)
+                    
+        else:
+            print("enetered")
+            print(peptides[i])
+            children.append(peptides[i])
+            children.append(peptides[i+1])
+    return(children)
+
+
+
+#METHOD1:
+## this one will split in a difffernt way
+### pep1: 'R-AT-PFV'  lets say CuttingPoint=1  child1: 'R-NP-PFV'  
+### pep2: 'S-NP-ELY'           CuttingPoint2=3 child2: 'S-AT-ELY'
+
+#METHOD2:
+## this one will split in half so
+### pep1: 'RAT-PFV'   child1: 'RAT-ELY'
+### pep2: 'SNP-ELY'   child2: 'SNP-PFV'
+
+def crossover_half(peptides):
+    children = []
+    for i in range(0,len(peptides),2):
+        prob = random.random()
+        if prob<=crossoverProbability:
+            cuttingPoint=int(len(peptides[i])/2)
+            child1=peptides[i][0:cuttingPoint+1]+peptides[i+1][cuttingPoint+1:]
+            child2=peptides[i+1][0:cuttingPoint+1]+peptides[i][cuttingPoint+1:]
+            children.append(child1)
+            children.append(child2)           
         else:
             children.append(peptides[i])
             children.append(peptides[i+1])
-    if tmp ==1:
-        children.append(peptides[-1])
-    return(children)
+    return (children)
 
 
 def mutate(peptides):
@@ -125,15 +163,16 @@ def mutate(peptides):
     return (mutatedPeptides)
     
 
-
-
 ###################
 #generate 100 random individuals
 #randomlist=create_seq(100,6)
 
 crossoverProbability=0.8
 nvar=6
-maxFitness=-12
+maxFitness=-12 ### change to alanine
+elitismRate=0.6
+populationSize=100
+
 randomlist={'RQMSFH': 2,
  'MRYSWF': 4, 'HSAQYG': 2, 'IDTRVM': 3, 'KFPAIQ': 3, 'REKPSA': 1, 'WMQHIY': 4, 'YAQGVE': 3, 'QHDKAN': 1, 'QYCPGT': 1, 'VCMPWR': 3, 'PNHTWG': 1, 'VFENDP': 2, 'WQNLTC': 2, 'KWNGVE': 2, 'NTLGFY': 3, 'REPGAQ': 1, 'DCKIHR': 1, 'CKYHDI': 2, 'FLNGCM': 3, 'PIKLGA': 3, 'QNMVKC': 2, 'CRLMDT': 2, 'MKILTE': 3, 'YTCVAP': 3, 'KSNMQH': 1, 'HIERYD': 2, 'DQWCSN': 1, 'LKEIMP': 3, 'GDPSQN': 0, 'RCFMGY': 3, 'RIEYKP': 2, 'ARIMES': 3, 'RATPFV': 3, 'NKYSDA': 2, 'AKHDVF': 3, 'RAPTKY': 2, 'DFEQHC': 1, 'VMCKLG': 3, 'DKCEGM': 1, 'MKVACE': 3, 'QEDTHP': 0, 'RFCHTV': 2, 'EVTLCS': 2, 'DNRPIG': 1, 'LYFTRG': 3, 'NFLPAM': 4,
  'ANMKEV': 3,   'HAFSEG': 2, 'RSCEMA': 2, 'EVHKWF': 3, 'ICMQKD': 2, 'EIYTLR': 3, 'SCDWQY': 2, 'NLWPIC': 3,
@@ -147,13 +186,24 @@ randomlist={'RQMSFH': 2,
 
 ## calculate fitness
 data=compute_fitness("C:\\Users\\user\\Desktop\\hsp70\\alanine_mut_buildmodel\\GA_round1\\Summary_GA_file.txt")    
-select=selection(data.peptideseq, data.Interaction_Energy, 100)
-children=crossover(select)
-print(children)
+## elitism?
+parents=data.peptideseq
+fitness=data.Interaction_Energy
+eliteIndividuals = parents[0: math.ceil(populationSize*elitismRate)]
+toSelect = populationSize - math.ceil(populationSize*elitismRate)
+
+# if we want to use the entire data
+#select=selection(data.peptideseq, data.Interaction_Energy, toSelect)
+# use the data that are not considered "elite"
+select=selection(parents[math.ceil(populationSize*elitismRate):],fitness[math.ceil(populationSize*elitismRate):], toSelect)
+
+children_new=crossover_half(select)
+print(children_new)
+
+#nextGen=list(eliteIndividuals)+ list(children_new)
+
+
 #new_pop=mutate(children)
 # for i in select:
 #     print(data[data.peptideseq==i])
-    
 
-
-    
